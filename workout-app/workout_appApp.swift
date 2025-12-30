67015b79f40863993f576e54f102b96357006137
davisdeatonphotography <davis.deaton@verizon.net>
2025-12-30 11:29:01 -0700

    Initial Commit

---
+++ workout-app/workout_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  workout_appApp.swift
+//  workout-app
+//
+//  Created by Davis Deaton on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct workout_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
